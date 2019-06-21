67780bfa523e5f4eeb696674316756846e1d912a
费锋 <feng.fei@trinasolar.com>
2019-06-21 21:37:52 +0800

    Initial commit

---
+++ src/java/proxy/jdk/CustomerStaticProxy.java
@@ -0,0 +1,23 @@
+package proxy.jdk;
+
+/**
+ * @author ：dfghhj
+ * @date ：Created in 2019/6/21 16:34
+ * @description:
+ */
+public class CustomerStaticProxy implements ICustomer {
+
+    private Customer customer = new Customer();
+
+    @Override
+    public void buy(String s) {
+        System.out.println("静态代理前置操作");
+        customer.buy(s);
+        System.out.println("静态代理后置操作");
+    }
+
+    public static void main(String[] args) {
+        CustomerStaticProxy customerStaticProxy = new CustomerStaticProxy();
+        customerStaticProxy.buy("衣服");
+    }
+}
---
+++ src/java/proxy/jdk/ICustomer.java
@@ -0,0 +1,10 @@
+package proxy.jdk;
+
+/**
+ * @author ：dfghhj
+ * @date ：Created in 2019/6/21 16:31
+ * @description:
+ */
+public interface ICustomer {
+    void buy(String s);
+}
